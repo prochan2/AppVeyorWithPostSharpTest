d259077c47f278573370933aaa78daa7c3f77886
Antonín Procházka <antonin@postsharp.net>
2017-11-27 15:28:14 +0100

    PostSharp added.

--- AppVeyorWithPostSharpTest/Program.cs
+++ AppVeyorWithPostSharpTest/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PostSharp.Aspects;
+using PostSharp.Serialization;
 
 namespace AppVeyorWithPostSharpTest
 {
     class Program
     {
+        [SayHello]
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello PostSharp!");
+        }
+    }
+
+    [PSerializable]
+    class SayHelloAttribute : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Hello C#!");
         }
     }
 }
